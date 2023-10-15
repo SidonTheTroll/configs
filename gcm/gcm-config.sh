0bb18f88622263c2a1a83ec12112557684f6f11b
Sidon_OO0 <70560640+SidonTheTroll@users.noreply.github.com>
2023-10-15 20:54:39 +0530

    Update gcm-config.sh

--- gcm/gcm-config.sh
+++ gcm/gcm-config.sh
@@ -3,8 +3,8 @@
 # PLACE BOTH .txt AND .sh FILES IN ~/GCM/ FOLDER
 
 # Read credentials from the plain text file
-USERNAME=$(grep '^username=' ~/gcm/gcm-config.txt | cut -d '=' -f 2)
-PASSWORD=$(grep '^password=' ~/gcm/gcm-config.txt | cut -d '=' -f 2)
+USERNAME=$(grep '^username=' $HOME/gcm/gcm-config.txt | cut -d '=' -f 2)
+PASSWORD=$(grep '^password=' $HOME/gcm/gcm-config.txt | cut -d '=' -f 2)
 
 # Output credentials in the format expected by Git
 echo "username=$USERNAME"
